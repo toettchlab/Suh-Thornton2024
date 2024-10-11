bbf395ffd634051b40de092cb71da7e265b58c5b
toettchlab <50968258+toettchlab@users.noreply.github.com>
2024-10-11 09:30:30 -0400

    Update FOR_PAPER_Fig2G.m
    Better comments!

--- FOR_PAPER_Fig2G.m
+++ FOR_PAPER_Fig2G.m
@@ -1,10 +1,16 @@
-%% Make a uniform initial pattern of cells
+%% Initialize a clean workspace
+clear all, close all
+
+%% PART 1: SIMULATE THE PATTERN
+% This code simulates migration in response to the circular pattern from
+% Figure 2E-G.
+
+% 1.1 Make a uniform initial pattern of cells
 ic = ones(101,101); % a 101 x 101 discretized grid of cells
 
 imshow(ic)
 
-%%
-% Construct illumination circle centered at the origin
+% 1.2 Construct an illumination circle centered at the origin
 
 R = 9.35; % 500 um / 5400 um * 101 units = 9.35 unit diameter
 
@@ -20,7 +26,7 @@ imagesc(u)
 axis equal
 pattern = u;
 
-%% Simulate with "outgrowth" initial conditions
+% 1.3 Simulate this model
 N = 101;
 L = 5400;  % simulation width in um (6 mm = 6,000 um)
 tF = 1920; % simulation time in min (32 h = 1,920 min)
@@ -37,20 +43,28 @@ yv = interp1(t, y, tv);
 
 save Fig2G_simulation_results L N tF tv yv
 
-return
+%% PLOT RESULTS
+% This code plots the simulation related to the experimental data of Figure 2G.
 
-%%
 load Fig2G_simulation_results
 
 xv = linspace(0,L,N);
 [X Y] = meshgrid(xv, xv);
+
+% Get 20 hour data
 i20 = find(tv >= 20*60, 1, 'first');
 Z20 = reshape(yv(i20,:),N,N);
+
+% Get 32 hour data
 i32 = find(tv >= 32*60, 1, 'first');
 Z32 = reshape(yv(i32,:),N,N);
+
+% Plot the 20 and 32 hour data as in Fig. 2G
 xx = X(51,51:end)-L/2;
 plot(xx, Z20(51,51:end), xx, Z32(51,51:end))
 xlabel('distance from center')
 ylabel('relative cell density')
+legend({'20 h' '32 h'}), legend boxoff
 set(gca, 'ylim', [0 2])
 set(gca, 'xlim', [0 1000])
+set(gcf, 'position', [360 290 360 310])
